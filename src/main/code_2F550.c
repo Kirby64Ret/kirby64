91ea3944f926eb45e0b65f48302b3e5a5e0b96e6
Faris Awan <farisawan.2000@gmail.com>
2024-10-30 21:21:41 -0400

    Regen src

---
+++ src/main/code_2F550.c
@@ -0,0 +1,25 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osLeoInterrupt.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osLeoAbnormalResume.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osLeoResume.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osMotorAccess.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/func_8002F1A8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/osMotorInit.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osSiCreateAccessQueue.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osSiGetAccess.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osSiRelAccess.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/osContInit.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osContGetInitData.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_2F550/__osPackRequestData.s")
---
+++ src/main/code_319F0.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_319F0/osPfsAllocateFile.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/code_319F0/func_80031124.s")
