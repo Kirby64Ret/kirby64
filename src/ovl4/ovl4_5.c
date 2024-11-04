eb239142888ee33e94e7cef9ac8b2bbbf766809a
Faris Awan <farisawan.2000@gmail.com>
2024-11-04 09:01:49 -0500

    split ovl4

---
+++ src/ovl4/ovl4_5.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159B00_ovl4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159B68_ovl4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159C40_ovl4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159CB8_ovl4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159D70_ovl4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159D78_ovl4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159DE8_ovl4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159ED8_ovl4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/ovl4/ovl4_5/func_80159EFC_ovl4.s")
