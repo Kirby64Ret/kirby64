8674d1397de8e7f6d3cbded6d75a071e9778360c
someone2639 <someone2639@gmail.com>
2026-08-16 22:47:18 -0400

    bring back shiftable filetables

---
+++ src/ovl1/filetable.c
@@ -0,0 +1,10 @@
+#include <ultra64.h>
+
+#include "assets/bank0.filetable.c"
+#include "assets/bank1.filetable.c"
+#include "assets/bank2.filetable.c"
+#include "assets/bank3.filetable.c"
+#include "assets/bank4.filetable.c"
+#include "assets/bank5.filetable.c"
+#include "assets/bank6.filetable.c"
+#include "assets/bank7.filetable.c"
